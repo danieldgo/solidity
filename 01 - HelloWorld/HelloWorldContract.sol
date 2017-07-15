2541624a895286933ff11ea5555016479d748412
Daniel Diego <daniel.diego@definityfirst.com>
2017-07-14 21:55:48 -0500

    Simple contract end of first turorial

---
+++ 01 - HelloWorld/HelloWorldContract.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.0;
+
+contract HelloWorldContract{
+    string word = "Hello World";
+}
+
+//run this tests at https://ethereum.github.io/browser-solidity/#version=soljson-v0.4.13+commit.fb4cb1a.js
